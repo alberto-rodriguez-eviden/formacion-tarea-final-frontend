17ab6ddbd6b1bf5c66e58208d3b28228f6828511
Alberto Rodriguez Muelas <alberto.rodriguez.external@eviden.com>
2025-05-05 15:27:57 +0200

    fixed duplicated button on client form and added name filter on client list

--- src/app/clients-list/clients-list.component.ts
+++ src/app/clients-list/clients-list.component.ts
@@ -4,15 +4,25 @@ import { Client } from '../client/client';
 import { Observable, of } from 'rxjs';
 import { ClientService } from '../client/client.service';
 import { RouterLink } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
     selector: 'app-clients-list',
-    imports: [CommonModule, RouterLink],
+    imports: [CommonModule, RouterLink, FormsModule],
     templateUrl: './clients-list.component.html'
 })
 export class ClientsListComponent implements OnInit {
 
     clientList: Client[] = [];
+
+    filter: string = ""
+
+    filteredList = ():Client[] => this.clientList.filter(client => {
+        let clientName = (`${client.firstName} ${client.lastName}`).toLowerCase()
+        let filterName = this.filter.toLowerCase()
+
+        return clientName.includes(filterName)
+    })
 
     constructor(private clientService: ClientService) { }
 
